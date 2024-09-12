ba6ccb3e1580ae7097a0a9f4b3c982470ecafd95
Mikee0102 <mv236648@gmail.com>
2024-09-12 13:56:49 -0600

    03 Variables let y const

---
+++ concepts/variables.js
@@ -0,0 +1,18 @@
+// Declaración de variables con let y const
+// var: en desuso
+
+// Variables let: cambiarán de valor
+let age; // declaración
+console.log(age);
+age = 23; //inicialización
+console.log(age);
+
+let name = "Juaquin"; // declaración e inicialización
+console.log(name);
+name = "Pancho";
+console.log(name);
+
+// constantes const: no cambiarán de valor
+const PI = 3.1416;
+// PI = 3.15;
+console.log(PI);
