82b053eb986a967b773f67a8ea4d95df415877f9
Mikee0102 <mv236648@gmail.com>
2024-09-12 13:31:18 -0600

    01 Tipos de Dato Javascript

---
+++ concepts/datatypes.js
@@ -0,0 +1,12 @@
+// Tipos de Dato en Javascript
+// numericos (enteros, decimales)
+console.log(23)
+console.log(typeof 23)
+console.log(3.1416)
+console.log(typeof 3.1416)
+
+// booleanos (true, false)
+console.log(true);
+console.log(typeof true);
+console.log(false);
+console.log(typeof false);
