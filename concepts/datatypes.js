79ca6672aab5211f6e03e5cddf360f68c11d9d07
Mikee0102 <mv236648@gmail.com>
2024-09-12 13:45:14 -0600

    02 Operadores

--- concepts/datatypes.js
+++ concepts/datatypes.js
@@ -10,3 +10,12 @@ console.log(true);
 console.log(typeof true);
 console.log(false);
 console.log(typeof false);
+
+console.log ('Helo');
+console.log (typeof 'Hello');
+
+console.log(undefined);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof [1, 2, 3]);
+console.log(typeof { name: 'John'});
---
+++ concepts/operators.js
@@ -0,0 +1,35 @@
+// Operadores matemáticos
+// + y -
+// * y /
+// % y ** Módulo y potencia
+console.log (2 + 2);
+console.log (2 - 2);
+console.log (2 * 2);
+console.log (2 / 2);
+console.log (2 % 2);
+console.log (2 ** 2);
+
+// Operadores de asignación
+let a = 10;
+a += 5;
+a -= 5;
+a *= 5;
+a /= 5;
+a %= 5;
+a **= 5;
+console.log(a);
+
+// Operadores de Comparación
+console.log(2 > 2);
+console.log(2 < 2);
+console.log(2 >= 2);
+console.log(2 <= 2);
+console.log(2===2);
+console.log(2 == 2);
+console.log(2 != 2);
+console.log(2 !== 2);
+
+//Operadores logicos
+console.log(true && true); //and
+console.log(true || false); //or
+console.log(!true); //not
